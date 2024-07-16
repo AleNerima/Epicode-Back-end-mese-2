f90da6e0b1be8c1b0b061e05b7b6d8cfc4117f84
AleNerima <ale95polioni@gmail.com>
2024-07-16 16:15:10 +0200

    aggiunte per login

---
+++ Settimana 1/DittaSpedizioniApp/WebApplication1/Services/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using DittaSpedizioniApp.Models; // Assicurati di importare il namespace corretto per ApplicationUser
+using System.Threading.Tasks;
+
+namespace DittaSpedizioniApp.Services
+{
+    public interface IAuthService
+    {
+        Task<ApplicationUser> GetUserByIdAsync(string userId);
+        // Aggiungi altri metodi necessari per la gestione dell'autenticazione
+    }
+}
